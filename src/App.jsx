c5415125bbd49ff2af67ed46216add217e785768
Henry Duong <henryvinhd@gmail.com>
2020-03-05 17:20:47 -0500

    fixed grid stuff works now

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import HomePage from './Homepage';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import buffLogo from './buff_logo.png';
+import 'semantic-ui-css/semantic.min.css';
 
 
 
 class App extends Component {
   render(){
   return (
-    
-    <Grid >
+    <div>
+     <Header as='h1' className='topHeader'> <Image src={buffLogo} size='Large' />  </Header>
+    <Grid>
+      
       <Grid.Row>
-      <Header as='h1' className='topHeader'> <Image src={buffLogo} size='small' />  </Header>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column width={3}>
+        <Grid.Column width={6}>
           <NavBar />
         </Grid.Column>
         <Grid.Column width={10}>
@@ -26,14 +26,12 @@ class App extends Component {
           <div class="ui focus input"><input type="text" placeholder="Password" /></div>
           <Button>Login</Button>
         </Grid.Column>
-        <Grid.Column width={3}>
-          <Image src='/images/wireframe/image.png' />
-        </Grid.Column>
       </Grid.Row>
       <Grid.Row>
         <Footer />
       </Grid.Row>
     </Grid>
+    </div>
     
   )
   }
