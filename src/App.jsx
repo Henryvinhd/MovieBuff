efdaa7aa0587eaa6357003fb1bbb3a684367b224
Henry Duong <henryvinhd@gmail.com>
2020-03-05 15:15:11 -0500

    sectioned off stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Header, Button} from 'semantic-ui-react';
 import './App.css';
+import HomePage from './Homepage';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
-function App() {
+
+
+class App extends Component {
+  render(){
   return (
     <div className="App">
+      <HomePage />
+      <NavBar />
       <Header as='h1'>MovieBuff</Header>
         
         <div class="ui focus input"><input type="text" placeholder="Username" /></div>
         <div class="ui focus input"><input type="text" placeholder="Password" /></div>
         <Button>Login</Button>
-      
+      <Footer />
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <header>
+        <ul id="headerButtons">
+          <li className="navButton">Home</li>
+        </ul>
+      </header>
+    )
+  }
+}
+export default NavBar;
