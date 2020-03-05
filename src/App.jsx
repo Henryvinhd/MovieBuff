46ec78ade83f1e5cc2befb01669b3c2289f8df36
Henry Duong <henryvinhd@gmail.com>
2020-03-05 16:55:43 -0500

    new changes grid/navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
 import React, {Component} from 'react';
-import {Header, Button} from 'semantic-ui-react';
+import {Header, Button, Image, Grid} from 'semantic-ui-react';
 import './App.css';
 import HomePage from './Homepage';
 import NavBar from './NavBar';
 import Footer from './Footer';
+import buffLogo from './buff_logo.png';
 
 
 
 class App extends Component {
   render(){
   return (
-    <div className="App">
-      <HomePage />
-      <NavBar />
-      <Header as='h1'>MovieBuff</Header>
-        
-        <div class="ui focus input"><input type="text" placeholder="Username" /></div>
-        <div class="ui focus input"><input type="text" placeholder="Password" /></div>
-        <Button>Login</Button>
-      <Footer />
-    </div>
-  );
+    
+    <Grid >
+      <Grid.Row>
+      <Header as='h1' className='topHeader'> <Image src={buffLogo} size='small' />  </Header>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={3}>
+          <NavBar />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <HomePage />
+          <div class="ui focus input"><input type="text" placeholder="Username" /></div>
+          <div class="ui focus input"><input type="text" placeholder="Password" /></div>
+          <Button>Login</Button>
+        </Grid.Column>
+        <Grid.Column width={3}>
+          <Image src='/images/wireframe/image.png' />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Footer />
+      </Grid.Row>
+    </Grid>
+    
+  )
   }
 }
+
 
 export default App;
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
 class NavBar extends Component {
   render() {
     return (
-      <header>
-        <ul id="headerButtons">
-          <li className="navButton">Home</li>
-        </ul>
-      </header>
+    <ul>
+      <li><a href="default.asp">Home</a></li>
+      <li><a href="news.asp">News</a></li>
+      <li><a href="contact.asp">Contact</a></li>
+      <li><a href="about.asp">About</a></li>
+    </ul>
     )
   }
 }
