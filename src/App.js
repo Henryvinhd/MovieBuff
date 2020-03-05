39c01df693abd2e99d2cfef2d310502aba55760b
Henry Duong <henryvinhd@gmail.com>
2020-03-05 10:27:53 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Header, Button} from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header as='h1'>MovieBuff</Header>
+        
+        <div class="ui focus input"><input type="text" placeholder="Username" /></div>
+        <div class="ui focus input"><input type="text" placeholder="Password" /></div>
+        <Button>Login</Button>
+      
     </div>
   );
 }
