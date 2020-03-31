0acb4090de52b62a31ba13a560b60ebb13bd27f0
Brandon's Laptop <bfournier4805@gmail.com>
2020-03-31 10:37:40 -0400

    merged with updated  version

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,10 @@ import Login from './Login';
 import NavBar from './NavBar';
 import buffLogo from './Images/newlogo.png';
 import 'semantic-ui-css/semantic.min.css';
-<<<<<<< HEAD
 import UsersSetting from './UsersSettings';
-=======
 import MovieProfile from './MovieProfile';
 import SearchPage from './SearchPage';
 import background from './Images/dark-honeycomb.png'
->>>>>>> 5a0ba2c57e06621314d4b65b966b1aeb4fe53be3
 
 class App extends Component {
   constructor(props) {
@@ -51,29 +48,6 @@ class App extends Component {
     })
   }
 
-<<<<<<< HEAD
-class App extends Component {
-  render(){
-  return (
-    <div>
-     <div className='topHeader'> <Image src={buffLogo} size='small' className='center'/>  </div>
-      
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={4} >
-            <NavBar /> 
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <UsersSetting />
-          </Grid.Column>
-        </Grid.Row> 
-      </Grid> 
-      
-
-    </div>
-    
-  )
-=======
   render() {
    
     return (
