f53766b38f98d4eb0513276a54e95ff007014be0
epetrarca5479 <44585749+epetrarca5479@users.noreply.github.com>
2020-04-28 19:41:23 -0400

    Latest

--- src/components/SearchPageComponents/GridCell.jsx
+++ src/components/SearchPageComponents/GridCell.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Divider, Image, Label, Segment, Container} from 'semantic-ui-react';
+import ContentProfile from '../Profile/ContentProfile'
 
 class GridCell extends Component {
     constructor(props) {
@@ -14,7 +15,9 @@ class GridCell extends Component {
                         </Label>
                 </Segment>
                 <Container style = {{height: 125, backgroundColor: 'white'}}>
-                    <Image src={this.props.art} href = {this.props.art}/>
+                    {/* <Image src={this.props.art} href = {"http://api-public.guidebox.com/v2/shows/" + this.props.id + "?api_key=39145758a7c7ad3266d0a97c13643cecaeb109e1"}/> */}
+                    {/* <a target="_blank" href={"http://api-public.guidebox.com/v2/shows/" + this.props.id + "?api_key=39145758a7c7ad3266d0a97c13643cecaeb109e1"} class="ui medium image">   <img src={this.props.art}/> </a> */}
+                    <a target="_blank" href={ContentProfile} class="ui medium image">   <img src={this.props.art}/> </a>
                 </Container>
                     {/* <Label as ='a' color = 'red' basic size = 'small' style = {{margin: 10}}> {this.props.id} </Label> */}
                 <Segment rasied textAlign = 'center' style = {{height: 25, width: 190}}>
