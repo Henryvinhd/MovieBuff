0d54ee1a2558627c2f9520f4cc1c3fa8d59803d1
henryvinhd <henryvinhd@gmail.com>
2020-04-29 20:33:36 -0400

    fixed some content

--- src/utils/contentData.js
+++ src/utils/contentData.js
@@ -7,12 +7,12 @@ const contentData = [
   {
     title: "Connect",
     description:
-      "Our website also helps you connect with other users based on movie preferences."
+      "Our website also helps you connect with other users based on movie preferences. You can also add users to your friends list and follow them on their movie journey! "
   },
   {
-    title: "Review Movies",
+    title: "Reviews",
     description:
-      "This is also a space where you can leave your hot takes on the newest movies!"
+      "This is also a space where you can leave your hot takes on the newest movies or even see what your friends have to say on a specific show or movie!"
   },
   {
     title: "Privacy",
